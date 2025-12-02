e28036380bfdb2f7cd32a9bf6336c1663349aeac
Joseph <josephrock225@gmail.com>
2025-12-02 10:49:15 -0800

    finished day 2 (slow)

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,19 +1,21 @@
+use std::collections::HashSet;
+
 fn main() {
     let input: &str = include_str!("./day02.txt");
     let pt1 = part_1(input);
-    // let pt2 = part_2(input);
+    let pt2 = part_2(input);
     println!("Part 1: {pt1}");
-    // println!("Part 2: {pt2}");
+    println!("Part 2: {pt2}");
 }
 
 fn part_1(input: &str) -> usize {
+    let mut total = 0;
     let ranges: Vec<&str> = input
         .strip_suffix("\n")
         .expect("Line ends with single \n char")
         .split(",")
         .collect();
 
-    let mut total = 0;
     for r in ranges {
         let range: Vec<usize> = r
             .split("-")
@@ -40,4 +42,47 @@ fn part_1(input: &str) -> usize {
         }
     }
     total
+}
+
+fn part_2(input: &str) -> usize {
+    let mut total = 0;
+    let ranges: Vec<&str> = input
+        .strip_suffix("\n")
+        .expect("Line ends with single \n char")
+        .split(",")
+        .collect();
+
+    for r in ranges {
+        let range: Vec<usize> = r
+            .split("-")
+            .map(|number| {
+                number
+                    .parse::<usize>()
+                    .expect("Input promised to be a number")
+            })
+            .collect();
+        let min: usize = range[0];
+        let max: usize = range[1];
+
+        for number in min..max + 1 {
+            if solve(number) {
+                total += number;
+            }
+        }
+    }
+
+    total
+}
+
+fn solve(number: usize) -> bool {
+    let num_string: Vec<char> = number.to_string().chars().collect();
+    let mid = num_string.len() / 2;
+
+    for i in 1..mid + 1 {
+        let foo = num_string.rchunks(i).all(|chunk| *chunk == num_string[..i]);
+        if foo {
+            return true;
+        }
+    }
+    false
 }
