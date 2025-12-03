6e4b32eed7da744884a7e1f5347f331b816a0ed3
Joseph <josephrock225@gmail.com>
2025-12-02 19:34:19 -0800

    Clean part 1, part 2 is wrong

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,15 +1,15 @@
 fn main() {
     let input: &str = include_str!("./day01.txt");
     let pt1 = part_1(input);
-    // let pt2 = part_2(input);
+    let pt2 = part_2(input);
     println!("Part 1: {pt1}");
-    // println!("Part 2: {pt2}");
+    println!("Part 2: {pt2}");
 }
 
 fn part_1(input: &str) -> usize {
     let mut dial: isize = 50;
     let mut count: usize = 0;
-    
+
     for line in input.lines() {
         let c = line.chars().next().unwrap();
         let mut distance: isize = line[1..].parse().unwrap();
@@ -17,22 +17,35 @@ fn part_1(input: &str) -> usize {
         if c == 'L' {
             distance = distance * -1;
         }
-
         dial += distance;
-
-        while dial < 0 {
-            dial = 100 + dial;
-        }
-        while dial > 99 {
-            dial = dial - 100;
-        }
-
-        dbg!(dial);
+        dial = dial % 100;
 
         if dial == 0 {
             count += 1;
         }
+    }
+    count
+}
 
+fn part_2(input: &str) -> usize {
+    let mut dial: isize = 50;
+    let mut count: usize = 0;
+
+    for line in input.lines() {
+        let c = line.chars().next().unwrap();
+        let mut distance: isize = line[1..].parse().unwrap();
+
+        if c == 'L' {
+            distance = distance * -1;
+        }
+        dial += distance;
+        let turns = (dial / 100).abs();
+        count += turns as usize;
+        dial = dial % 100;
+
+        if dial == 0 {
+            count += 1;
+        }
     }
     count
 }
