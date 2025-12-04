c582e2b21479e1e991230dbafc4cb020cffd50d7
Joseph <josephrock225@gmail.com>
2025-12-04 10:54:00 -0800

    Clean neighbor_count()

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -51,21 +51,25 @@ fn part_2(input: &str) -> usize {
     })
 }
 
-#[rustfmt::skip]
 fn neighbor_count(grid: &Vec<Vec<char>>, paper: &char, x: usize, y: usize) -> usize {
+    let neighbor = |xi: usize, yi: usize| -> bool {
+        grid.get(yi)
+            .is_some_and(|line: &Vec<char>| line.get(xi) == Some(&paper))
+    };
+
     let n_border = y == 0;
     let s_border = y == grid.len() - 1;
     let e_border = x == grid.get(0).expect("0th index exists, trust").len() - 1;
     let w_border = x == 0;
 
-    let n: bool = !n_border && grid.get(y-1).is_some_and(|line| line.get(x) == Some(&paper));
-    let s: bool = !s_border && grid.get(y+1).is_some_and(|line| line.get(x) == Some(&paper));
-    let e: bool = !e_border && grid.get(y).is_some_and(|line| line.get(x+1) == Some(&paper));
-    let w: bool = !w_border && grid.get(y).is_some_and(|line| line.get(x-1) == Some(&paper));
-    let ne: bool = !n_border && !e_border && grid.get(y-1).is_some_and(|line| line.get(x+1) == Some(&paper));
-    let nw: bool = !n_border && !w_border && grid.get(y-1).is_some_and(|line| line.get(x-1) == Some(&paper));
-    let se: bool = !s_border && !e_border && grid.get(y+1).is_some_and(|line| line.get(x+1) == Some(&paper));
-    let sw: bool = !s_border && !w_border && grid.get(y+1).is_some_and(|line| line.get(x-1) == Some(&paper));
+    let n: bool = !n_border && neighbor(x, y - 1);
+    let s: bool = !s_border && neighbor(x, y + 1);
+    let e: bool = !e_border && neighbor(x + 1, y);
+    let w: bool = !w_border && neighbor(x - 1, y);
+    let ne: bool = !n_border && !e_border && neighbor(x + 1, y - 1);
+    let nw: bool = !n_border && !w_border && neighbor(x - 1, y - 1);
+    let se: bool = !s_border && !e_border && neighbor(x + 1, y + 1);
+    let sw: bool = !s_border && !w_border && neighbor(x - 1, y + 1);
 
     usize::from(n)
         + usize::from(s)
