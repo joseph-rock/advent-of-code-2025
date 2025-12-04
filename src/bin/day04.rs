2607ac4cae51919abd27b4e503bab383892affb0
Joseph <josephrock225@gmail.com>
2025-12-04 10:18:36 -0800

    Complete day 4

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -1,12 +1,14 @@
 fn main() {
     let input: &str = include_str!("./day04.txt");
     let pt1 = part_1(input);
-    // let pt2 = part_2(input);
+    let pt2 = part_2(input);
     println!("Part 1: {pt1}");
-    // println!("Part 2: {pt2}");
+    println!("Part 2: {pt2}");
 }
 
 fn part_1(input: &str) -> usize {
+    // Collecting then immediately iterating after, but I "need" to reference
+    // the grid (to count neighbors). Do better.
     let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
     let mut count = 0;
     let paper = '@';
@@ -14,7 +16,7 @@ fn part_1(input: &str) -> usize {
     for (y, line) in grid.clone().into_iter().enumerate() {
         for (x, character) in line.into_iter().enumerate() {
             if character == paper {
-                if neighbor_count(&grid, x, y) < 4 {
+                if neighbor_count(&grid, &paper, x, y) < 4 {
                     count += 1;
                 }
             }
@@ -24,9 +26,33 @@ fn part_1(input: &str) -> usize {
     count
 }
 
-fn neighbor_count(grid: &Vec<Vec<char>>, x: usize, y: usize) -> usize {
+fn part_2(input: &str) -> usize {
+    let mut grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
     let paper = '@';
+    let removed = 'x';
+    let mut paper_removed = true;
 
+    while paper_removed {
+        paper_removed = false;
+        for (y, line) in grid.clone().into_iter().enumerate() {
+            for (x, character) in line.into_iter().enumerate() {
+                if character == paper {
+                    if neighbor_count(&grid, &paper, x, y) < 4 {
+                        grid[y][x] = removed;
+                        paper_removed = true;
+                    }
+                }
+            }
+        }
+    }
+
+    grid.into_iter().fold(0, |sum, line| {
+        sum + line.into_iter().filter(|c| c == &removed).count()
+    })
+}
+
+#[rustfmt::skip]
+fn neighbor_count(grid: &Vec<Vec<char>>, paper: &char, x: usize, y: usize) -> usize {
     let n_border = y == 0;
     let s_border = y == grid.len() - 1;
     let e_border = x == grid.get(0).expect("0th index exists, trust").len() - 1;
