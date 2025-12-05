b6133d9665aca1583c37402c807e15a4d074e657
Joseph <josephrock225@gmail.com>
2025-12-05 07:34:50 -0800

    Init day 5

---
+++ src/bin/day05.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let input: &str = include_str!("./day05.txt");
+    let pt1 = part_1(input);
+    println!("Part 1: {pt1}");
+    // let pt2 = part_2(input);
+    // println!("Part 2: {pt2}");
+}
+
+fn part_1(input: &str) -> usize {
+    let mut count = 0;
+    count
+}
