b2a3b46c565893b8bed34fdc46695c48adb3a0a6
Joseph <josephrock225@gmail.com>
2025-12-05 20:41:35 -0800

    Part 2 Complete

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -2,11 +2,11 @@ fn main() {
     let input: &str = include_str!("./day05.txt");
     let pt1 = part_1(input);
     println!("Part 1: {pt1}");
-    // let pt2 = part_2(input);
-    // println!("Part 2: {pt2}");
+    let pt2 = part_2(input);
+    println!("Part 2: {pt2}");
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Range {
     min: usize,
     max: usize,
@@ -47,4 +47,58 @@ fn part_1(input: &str) -> usize {
     }
 
     count
+}
+
+fn part_2(input: &str) -> usize {
+    let mut count = 0;
+    let (mut ranges, _) = parse_input(input);
+    let mut condensed_ranges = ranges.clone();
+
+    while let Some(range) = ranges.pop() {
+        condensed_ranges = condense(range, condensed_ranges);
+    }
+
+    for range in condensed_ranges {
+        count += range.max - range.min + 1;
+    }
+
+    count
+}
+
+fn condense(range: Range, ranges: Vec<Range>) -> Vec<Range> {
+    let mut return_ranges: Vec<Range> = vec![];
+    let mut range_clone = range.clone();
+
+    for check_range in ranges {
+        if overlap(&range_clone, &check_range) {
+            range_clone.min = lowest(range_clone.min, check_range.min);
+            range_clone.max = highest(range_clone.max, check_range.max);
+        } else {
+            return_ranges.push(check_range);
+        }
+    }
+
+    return_ranges.push(range_clone);
+    return_ranges
+}
+
+fn overlap(left: &Range, right: &Range) -> bool {
+    if left.max < right.min || left.min > right.max {
+        return false;
+    }
+    true
+}
+
+fn highest(left: usize, right: usize) -> usize {
+    if left >= right {
+        return left;
+    }
+    right
+}
+
+fn lowest(left: usize, right: usize) -> usize {
+    if left <= right {
+        return left;
+    }
+    right
 }
