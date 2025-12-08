c3e1d58913a30c60b4d4e88ca4fb76f9d50a492d
Joseph <josephrock225@gmail.com>
2025-12-08 10:08:48 -0800

    complete part 1

---
+++ src/bin/day07.rs
@@ -0,0 +1,33 @@
+use std::collections::HashSet;
+
+fn main() {
+    let input: &str = include_str!("./day07.txt");
+    let pt1 = part_1(input);
+    println!("Part 1: {pt1}");
+    // let pt2 = part_2(input);
+    // println!("Part 2: {pt2}");
+}
+
+fn part_1(input: &str) -> usize {
+    let start_char = 'S';
+    let splitter = '^';
+    let mut indexes: HashSet<usize> = HashSet::new();
+    let mut total = 0;
+
+    for (line_num, line) in input.lines().enumerate() {
+        for (i, character) in line.chars().enumerate() {
+            if line_num == 0 && character == start_char {
+                indexes.insert(i);
+                continue;
+            }
+
+            if indexes.contains(&i) && character == splitter {
+                indexes.remove(&i);
+                indexes.insert(i-1);
+                indexes.insert(i+1);
+                total += 1;
+            }
+        }
+    }
+    total
+}
