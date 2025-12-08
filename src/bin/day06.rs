5ba0ead0a3bb9c38aa24e2e8ef58ef89d1438a76
Joseph <josephrock225@gmail.com>
2025-12-07 22:26:10 -0800

    finish part 2

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -21,9 +21,7 @@ fn part_1(input: &str) -> usize {
     for mut seq in puzzle {
         let operator = seq.pop().expect("Should end with a + or *");
 
-        let num_seq = seq
-            .into_iter()
-            .map(|num| num.parse::<usize>().unwrap());
+        let num_seq = seq.into_iter().map(|num| num.parse::<usize>().unwrap());
 
         total += match operator {
             "+" => num_seq.fold(0, |acc, num| acc + num),
@@ -36,38 +34,38 @@ fn part_1(input: &str) -> usize {
 }
 
 fn part_2(input: &str) -> usize {
-    let mut puzzle: Vec<Vec<&str>> = vec![];
-    for line in input.lines() {
-        for (i, unit) in line.split_whitespace().enumerate() {
-            match puzzle.get_mut(i) {
-                Some(column) => column.push(unit),
-                None => puzzle.push(vec![unit]),
+
+    // Rotate puzzle
+    let mut puzzle: Vec<Vec<char>> = vec![];
+    for (y, line) in input.lines().enumerate() {
+        for (x, character) in line.chars().rev().enumerate() {
+            if y == 0 {
+                puzzle.push(vec![character]);
+            } else {
+                puzzle[x].push(character);
             }
         }
     }
 
+    // Gross String to &str to usize
+    let mut num_seq: Vec<usize> = vec![];
     let mut total = 0;
-    for mut seq in puzzle {
-        let operator = seq.pop().expect("Should end with a + or *");
+    for line in puzzle {
+        let mut line_string: String = line.into_iter().collect();
 
-        let mut column_numbers: Vec<usize> = vec![];
-        for num_string in seq {
-            for (i, character) in num_string.chars().rev().enumerate() {
-                let number_part: usize = character.to_digit(10).unwrap() as usize;
-                match column_numbers.get_mut(i) {
-                    Some(number) => *number = *number * 10 + number_part,
-                    None => column_numbers.push(number_part),
-                }
-            }
+        if line_string.contains('+') || line_string.contains('*') {
+            let operator = line_string.pop().unwrap();
+            num_seq.push(*&line_string.trim().parse::<usize>().unwrap());
+            total += match operator {
+                '+' => num_seq.into_iter().fold(0, |acc, num| acc + num),
+                '*' => num_seq.into_iter().fold(1, |acc, num| acc * num),
+                _ => panic!("You got some weird operator"),
+            };
+            num_seq = vec![];
+        } else if !line_string.trim().is_empty() {
+            num_seq.push(*&line_string.trim().parse::<usize>().unwrap());
         }
-
-        dbg!(&column_numbers);
-        total += match operator {
-            "+" => column_numbers.into_iter().fold(0, |acc, num| acc + num),
-            "*" => column_numbers.into_iter().fold(1, |acc, num| acc * num),
-            _ => panic!("You got some weird operator"),
-        }
-
     }
+
     total
 }
