c55bf90c26f89d31269e9236fb5320fa9d40c8e2
Joseph <josephrock225@gmail.com>
2025-12-08 14:05:59 -0800

    finish part 2

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,27 +1,20 @@
 fn main() {
     let input: &str = include_str!("./day01.txt");
     let pt1 = part_1(input);
-    let pt2 = part_2(input);
     println!("Part 1: {pt1}");
+    let pt2 = part_2(input);
     println!("Part 2: {pt2}");
 }
 
 fn part_1(input: &str) -> usize {
-    let mut dial: isize = 50;
+    let mut dial: usize = 50;
     let mut count: usize = 0;
 
     for line in input.lines() {
         let direction = line.chars().next().unwrap();
-        let mut distance: isize = line[1..].parse().unwrap();
+        let distance: usize = line[1..].parse().unwrap();
 
-        // Make everything a right turn
-        if direction == 'L' {
-            let rotations = distance / 100;
-            let offset = (rotations + 1) * 100;
-            distance = offset - distance;
-        }
-        dial += distance;
-        dial = dial % 100;
+        (dial, _) = turn(direction, dial, distance);
 
         if dial == 0 {
             count += 1;
@@ -31,14 +24,45 @@ fn part_1(input: &str) -> usize {
 }
 
 fn part_2(input: &str) -> usize {
-    let mut dial: isize = 50;
-    let mut count: usize = 0;
+    let mut dial: usize = 50;
+    let mut zeros: usize = 0;
 
     for line in input.lines() {
-        let c = line.chars().next().unwrap();
-        let mut distance: isize = line[1..].parse().unwrap();
+        let direction = line.chars().next().unwrap();
+        let distance: usize = line[1..].parse().unwrap();
 
-        todo!();
+        let (new_dial, turns) = turn(direction, dial, distance);
+        dial = new_dial;
+        zeros += turns;
     }
-    count
+    zeros
+}
+
+fn turn(direction: char, start: usize, distance: usize) -> (usize, usize) {
+    match direction {
+        'R' => {
+            let dial = (start + distance) % 100;
+            let turns = (start + distance) / 100;
+            return (dial, turns);
+        }
+        'L' => {
+            let mut turns = 0;
+            let mut dial = 0;
+            let rem = distance % 100;
+
+            if rem > start {
+                dial = 100 - (rem - start);
+                if start != 0 {
+                    turns += 1;
+                }
+                turns += distance / 100;
+            } else {
+                dial = start - rem;
+                turns += (distance / 100) + (dial == 0) as usize;
+            }
+
+            return (dial, turns);
+        }
+        _ => panic!("bad direction in turn()"),
+    }
 }
